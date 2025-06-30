571a4ce0b8c0dccfdd766e75d0927d7cc7c7a329
stephane-klein <contact@stephane-klein.info>
2025-06-30 11:35:45 +0200

    WIP

--- scripts/push-openwebui-pipelines-docker.sh
+++ scripts/push-openwebui-pipelines-docker.sh
@@ -3,4 +3,5 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-docker push stephaneklein/openwebui-pipelines:latest
+#docker push stephaneklein/openwebui-pipelines:latest
+docker push stephaneklein/openwebui-pipelines:git-7f9f957-with-included-functions
